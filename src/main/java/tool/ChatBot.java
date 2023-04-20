b8db09582184473e34f9e2b88cb169dddd317cce
ibrahim140 <44937151+ibrahim140@users.noreply.github.com>
2023-04-19 19:02:50 -0700

    Update ChatBot.java

--- src/main/java/tool/ChatBot.java
+++ src/main/java/tool/ChatBot.java
@@ -10,11 +10,13 @@ public class ChatBot implements Runnable {
     private Socket socket;
     private volatile boolean isRunning = true;
     private int id;
-
+    private PrintWriter writer;
+    
     public ChatBot(String ip, int port, int id) {
         try {
             this.socket = new Socket(ip, port);
             this.id = id;
+            this.writer = new PrintWriter(socket.getOutputStream(), true);
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
             e.printStackTrace();
@@ -27,17 +29,23 @@ public class ChatBot implements Runnable {
             // We can use a printwriter to write messages to the client.
             // No buffered reader is necessary beacuse we do not need to display messages to
             // these bots.
-
+            String username = "Bot" + id;
+            writer.println("SIGNIN " + username);
             // Server is expecting initial signin message of "SIGNIN <username>"
 
             while (isRunning) {
                 // Send some message
                 // Sleep for a little to space messages out
+                writer.println(username + ": Test message from " + username);
+                Thread.sleep(2000); // 2 second sleep time
             }
 
             // Server is expecting ending signoff message of "SIGNOFF <username>"
+            writer.println("SIGNOFF " + username);
 
             // Close socket and printwriter
+            writer.close();
+            socket.close();
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
             System.err.println(e.getStackTrace());
@@ -57,6 +65,9 @@ public class ChatBot implements Runnable {
 
         // Read in each command line argument for ip, port, and number of bot clients to
         // create
+        ip = args[0]; // ip address
+        port = Integer.parseInt(args[1]); // port number
+        numClients = Integer.parseInt(args[2]); // number of bots/clients
 
         // We can save the ChatBots in this array so we can access them later and stop
         // them
@@ -65,8 +76,13 @@ public class ChatBot implements Runnable {
         /*
          * For the number of clients:
          * 1. instantiate and add each chatBot to the array
-         * 2. Create a start a new thread with the ChatBot class
+         * 2. Create a[nd] start a new thread with the ChatBot class
          */
+        for (int i = 0; i < numClients; i++) {
+            ChatBot bot = new ChatBot(ip, port, i);
+            userArray.add(bot);
+            new Thread(bot).start();
+        }
 
         // To stop the bots we can input a keyword or just wait for any input
         Scanner scanner = new Scanner(System.in);
@@ -77,6 +93,9 @@ public class ChatBot implements Runnable {
          * 1. set each chatBot in the array's isRunning variable to false, so it will
          * break out of the loop outputting text
          */
+        for (ChatBot bot : userArray) {
+            bot.isRunning = false;
+        }
 
         scanner.close();
     }
